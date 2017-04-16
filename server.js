47b4ec74045b218686d0ede31a567b1d3eb95951
Zach Hernandez <zkzooms@gmail.com>
2017-04-16 11:14:47 -0700

    change app to knockknock

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var http = require("http");
-var Poem = require ("poemify");
-var txtgen = require('txtgen');
+//var Poem = require ("poemify");
+//var txtgen = require('txtgen');
+var knockknock = require('knock-knock-jokes');
 
 var localport = process.env.PORT || 3200;
 function requestHandler(request, response){
-	let paragraph = txtgen.paragraph();
-  var p = new Poem(paragraph);
-  response.end(p.generate());
+  //let paragraph = txtgen.paragraph();
+  //var p = new Poem(paragraph);
+  //response.end(p.generate());
+  response.end(knockknock());
 }
 
 var server = http.createServer(requestHandler);
